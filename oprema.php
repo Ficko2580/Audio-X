1370c6c3bf1ed072e5979297f00aca5b79f6f789
Matic <matic.sulc@sers.si>
2018-02-21 16:46:51 +0100

    fixes

---
+++ oprema.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="navbar">
+  
+  <a href="/home.php">Domov</a>
+  <div class="dropdown">
+    <button class="dropbtn">Ozvocenje prireditev
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="/ozv-paketi.php">Paketi</a>
+      <a href="/mnenja.php">Mnenja</a>
+    </div>
+  </div> 
+  
+  <div class="dropdown">
+    <button class="dropbtn">Izposoja opreme 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="/izp-paketi.php">Paketi</a>
+      <a href="/oprema.php">Oprema</a>
+    </div>
+  </div> 
+  
+  <a href="o-nas.php">O nas</a>
+  <a href="kontakt.php">Kontakt</a>
+</div>
+<div class="centered-text">
+<h1> OPREMA </h1>
+<p> Coming soon! </p>
+</div>
+</body>
+</html>
